d05670b8ec35c111c6bf3773e9391c5cf893c46a
aidabah <aidasulayb@gmail.com>
2025-09-07 21:05:10 -0500

    - Intialize C++ development environment - Create Waypoint struct header file

---
+++ simulation/unreal/Source/FlyVR/Public/CsvWriter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CsvWriter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLYVR_API UCsvWriter : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
